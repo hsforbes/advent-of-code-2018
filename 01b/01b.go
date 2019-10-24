fb739f27f4b48654f2e361c820eb7a30d0b8fb03
Heather Forbes <heather_forbes@homedepot.com>
2019-10-24 17:25:43 -0500

    Spaghetti code way of looping input and checking for found values.

--- 01b/01b.go
+++ 01b/01b.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	fmt.Println("Are you ready to go?")
 
-	buffer, _ := ioutil.ReadFile("./input.txt")
+	buffer, _ := ioutil.ReadFile("./input2.txt")
 	lines := strings.Split(string(buffer), "\n")
 
 	total := 0
@@ -21,37 +21,48 @@ func main() {
 	// }
 
 	// for _, line := range strings.Split(string(buffer), "\n") {
+	loopCounter := 0
+
+	foundValues := []int{}
 
 	for i := 0; i < len(lines); i++ {
 		line := lines[i]
 		// fmt.Printf("Line: %s\n", line)
 
 		if len(line) == 0 {
-			continue
-		}
-
-		fmt.Printf("%s ", line)
-
-		lineString := string(line)
-		firstCharRune := []rune(lineString)[0]
-		// numberRune := []rune(lineString)[1:len(lineString)]
-		// numberInt := int(numberRune - '0')
-		numberString := lineString[1:len(lineString)]
-		numberInt, err := strconv.Atoi(numberString)
-		if err != nil {
-			panic(err)
-		}
-
-		// fmt.Printf("%d ", numberInt)
-
-		if "+" == string(firstCharRune) {
-			// fmt.Printf("Found plus   %c ", line[0])
-			total += numberInt
+			if loopCounter < 3 {
+				i = -1
+				loopCounter++
+			} else {
+				break
+			}
 		} else {
-			total -= numberInt
-		}
 
-		fmt.Printf("\t# current total: %d\n", total)
+			fmt.Printf("%s ", line)
+
+			lineString := string(line)
+			firstCharRune := []rune(lineString)[0]
+			// numberRune := []rune(lineString)[1:len(lineString)]
+			// numberInt := int(numberRune - '0')
+			numberString := lineString[1:len(lineString)]
+			numberInt, err := strconv.Atoi(numberString)
+			if err != nil {
+				panic(err)
+			}
+
+			// fmt.Printf("%d ", numberInt)
+
+			if "+" == string(firstCharRune) {
+				// fmt.Printf("Found plus   %c ", line[0])
+				total += numberInt
+			} else {
+				total -= numberInt
+			}
+
+			foundValues = append(foundValues, total)
+
+			fmt.Printf("\t# current total: %d\n", total)
+		}
 	}
 
 }

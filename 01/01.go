09414edc4c24a477bfb800acba31b40fdbde18c7
Heather Forbes <heather_forbes@homedepot.com>
2019-11-08 17:40:06 -0600

    Come up with a first solution. Maybe it's working. It runs for more than a few minutes.

--- 01/01.go
+++ 01/01.go
@@ -2,56 +2,16 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"strconv"
-	"strings"
+	"sort"
 )
 
-// Entry point
 func main() {
-	fmt.Println("Are you ready to go?")
 
-	buffer, _ := ioutil.ReadFile("./input.txt")
-	lines := strings.Split(string(buffer), "\n")
-
-	total := 0
-
-	// for i := 0; i < len(lines); i++ {
-	// 	fmt.Printf("Line\t%d:\t%s\n", i, lines[i])
-	// }
-
-	// for _, line := range strings.Split(string(buffer), "\n") {
-
-	for i := 0; i < len(lines); i++ {
-		line := lines[i]
-		// fmt.Printf("Line: %s\n", line)
-
-		if len(line) == 0 {
-			continue
-		}
-
-		fmt.Printf("%s ", line)
-
-		lineString := string(line)
-		firstCharRune := []rune(lineString)[0]
-		// numberRune := []rune(lineString)[1:len(lineString)]
-		// numberInt := int(numberRune - '0')
-		numberString := lineString[1:len(lineString)]
-		numberInt, err := strconv.Atoi(numberString)
-		if err != nil {
-			panic(err)
-		}
-
-		// fmt.Printf("%d ", numberInt)
-
-		if "+" == string(firstCharRune) {
-			// fmt.Printf("Found plus   %c ", line[0])
-			total += numberInt
-		} else {
-			total -= numberInt
-		}
-
-		fmt.Printf("\t# current total: %d\n", total)
-	}
-
+	foundValues := []int{3, 5, 7}
+	
+	newValue := 6
+	
+	suggInd := sort.SearchInts(foundValues, newValue)
+	
+	fmt.Printf("Suggested index:\t%d\n", suggInd)
 }
--- 01b/01b.go
+++ 01b/01b.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -11,7 +12,7 @@ import (
 func main() {
 	fmt.Println("Are you ready to go?")
 
-	buffer, _ := ioutil.ReadFile("./input2.txt")
+	buffer, _ := ioutil.ReadFile("./input.txt")
 	lines := strings.Split(string(buffer), "\n")
 
 	total := 0
@@ -21,47 +22,66 @@ func main() {
 	// }
 
 	// for _, line := range strings.Split(string(buffer), "\n") {
-	loopCounter := 0
 
 	foundValues := []int{}
 
-	for i := 0; i < len(lines); i++ {
-		line := lines[i]
-		// fmt.Printf("Line: %s\n", line)
+	for finished := false; finished == false; {
+		for i := 0; i < len(lines); i++ {
+			line := lines[i]
+			// fmt.Printf("%s ", line)
 
-		if len(line) == 0 {
-			if loopCounter < 3 {
-				i = -1
-				loopCounter++
+			if len(line) == 0 {
+				if finished == false {
+					i = -1
+				} else {
+					break
+				}
 			} else {
-				break
+
+				lineString := string(line)
+				firstCharRune := []rune(lineString)[0]
+				// numberRune := []rune(lineString)[1:len(lineString)]
+				// numberInt := int(numberRune - '0')
+				numberString := lineString[1:len(lineString)]
+				numberInt, err := strconv.Atoi(numberString)
+				if err != nil {
+					panic(err)
+				}
+
+				// fmt.Printf("%d ", numberInt)
+
+				if "+" == string(firstCharRune) {
+					// fmt.Printf("Found plus   %c ", line[0])
+					total += numberInt
+				} else {
+					total -= numberInt
+				}
+
+				suggestedIndex := sort.SearchInts(foundValues, total)
+
+				// Check for puzzle completion
+				if suggestedIndex != len(foundValues) {
+					if foundValues[suggestedIndex] == total {
+						finished = true
+						fmt.Printf("\n* * *\nFinished! First duplicate total:\t%d\n* * *\n", total)
+					}
+				}
+				// Otherwise, move on
+
+				// Put the new total in the right place in the sorted slice
+				foundValues = append(foundValues, total)
+				sort.Ints(foundValues)
+
+				fmt.Printf("\t# Current total: %d", total)
+
+				// fmt.Printf("\t# Found values len: %d", len(foundValues))
+				// fmt.Printf("\t# Found Values: %d", foundValues)
+				// for j := 0; j < len(foundValues); j++ {
+				// 	fmt.Printf(" %d,", foundValues)
+				// }
+
+				// fmt.Println()
 			}
-		} else {
-
-			fmt.Printf("%s ", line)
-
-			lineString := string(line)
-			firstCharRune := []rune(lineString)[0]
-			// numberRune := []rune(lineString)[1:len(lineString)]
-			// numberInt := int(numberRune - '0')
-			numberString := lineString[1:len(lineString)]
-			numberInt, err := strconv.Atoi(numberString)
-			if err != nil {
-				panic(err)
-			}
-
-			// fmt.Printf("%d ", numberInt)
-
-			if "+" == string(firstCharRune) {
-				// fmt.Printf("Found plus   %c ", line[0])
-				total += numberInt
-			} else {
-				total -= numberInt
-			}
-
-			foundValues = append(foundValues, total)
-
-			fmt.Printf("\t# current total: %d\n", total)
 		}
 	}
 

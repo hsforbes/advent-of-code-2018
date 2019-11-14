8762b5fc794fafbf0a41ed2de87c5e386e9f6d54
Heather Forbes <heather_forbes@homedepot.com>
2019-11-14 17:38:30 -0600

    Add some structure for day 2.

--- 02a/02a.go
+++ 02a/02a.go
@@ -18,14 +18,70 @@ func main() {
 
 	reader := bufio.NewReader(file)
 
+	linesWithExactlyTwoOfALetter, linesWithExactlyThreeOfALetter := 0, 0
+
 	for {
-		line, _, err := reader.ReadLine()
+		lineBytes, _, err := reader.ReadLine()
+		line := string(lineBytes)
 
 		if err == io.EOF {
 			break
 		}
 
-		fmt.Printf("%s \n", line)
+		fmt.Println();
+		fmt.Printf("\n%s", line)
+
+		lineAnswer := countTwoOrThreeRepeatedLetters(line)
+		
+		if(lineAnswer.hasExactlyTwoOfALetter) {
+			linesWithExactlyTwoOfALetter++
+		}
+		if(lineAnswer.hasExactlyThreeOfALetter) {
+			linesWithExactlyThreeOfALetter++
+		}
+
 	}
 
+	fmt.Printf("\nlinesWithExactlyTwoOfALetter:\t%d\t#\tlinesWithExactlyThreeOfALetter:\t%d", linesWithExactlyTwoOfALetter, linesWithExactlyThreeOfALetter)
+	fmt.Printf("\nChecksum:\t%d", linesWithExactlyTwoOfALetter * linesWithExactlyThreeOfALetter)
+
+}
+
+type LineAnswer struct {
+	hasExactlyTwoOfALetter, hasExactlyThreeOfALetter bool
+}
+
+func countTwoOrThreeRepeatedLetters(line string) LineAnswer {
+
+	findCharCounts(line)
+
+	/* 
+	look through chars for two or three counts
+	set lineAnswer
+	*/
+
+	lineAnswer := LineAnswer{true, true}
+	return lineAnswer
+}
+
+func findCharCounts(line string) []CharCount {
+	var charCounts []CharCount
+
+	for i:=0; i < len(line); i++ {
+		fmt.Printf("\t # Char: %s ", string(line[i]))
+	}
+
+	a := CharCount{13, 1}
+	// b := CharCount(13, 1)
+
+	charCounts = append(charCounts, a)
+	charCounts = append(charCounts, a)
+	// charCounts = append(charCounts, b)
+
+	return charCounts
+}
+
+type CharCount struct {
+	char byte
+	count int
 }

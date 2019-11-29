8ce7701baceb9f58b91c10212493ddd099c52999
Heather Forbes <heather_forbes@homedepot.com>
2019-11-29 11:10:29 -0600

    Finish 03a.

--- 03a/03a.go
+++ 03a/03a.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // Entry point
@@ -35,11 +37,15 @@ func main() {
 
 		fabric = addClaimToFabric(fabric, claim)
 
-		fmt.Printf("%s \n", line)
+		// fmt.Printf("%s \n", line)
 	}
 
 	// fmt.Printf("\nFinal fabric:\n")
 	// printFabric(fabric)
+
+	countOfMultipleClaims := countMultipleClaims(fabric)
+
+	fmt.Printf("\nCount of coordinates with multiple claims: %d", countOfMultipleClaims)
 
 }
 
@@ -48,18 +54,31 @@ type Claim struct {
 }
 
 func makeClaimFromLine(line string) Claim {
-	// segments := strings.Split(line, string(' '))
+	segments := strings.Split(line, string(' '))
 
-	return Claim{3, 2, 5, 4}
+	offsetsString := strings.ReplaceAll(segments[2], ":", "")
+	offsets := strings.Split(offsetsString, ",")
+
+	dimensions := strings.Split(segments[3], "x")
+
+	xOffset, err := strconv.Atoi(offsets[0])
+	yOffset, err := strconv.Atoi(offsets[1])
+	xDimension, err := strconv.Atoi(dimensions[0])
+	yDimension, err := strconv.Atoi(dimensions[1])
+	if err != nil {
+		panic(err)
+	}
+
+	return Claim{xOffset, yOffset, xDimension, yDimension}
 }
 
 func addClaimToFabric(fabric [][]int, claim Claim) [][]int {
 
-	for xIndex := claim.xOffset; xIndex < claim.xOffset+claim.xLength-1; xIndex++ {
-		for yIndex := claim.yOffset; yIndex < claim.yOffset+claim.yLength-1; yIndex++ {
+	for xIndex := claim.xOffset; xIndex < claim.xOffset+claim.xLength; xIndex++ {
+		for yIndex := claim.yOffset; yIndex < claim.yOffset+claim.yLength; yIndex++ {
 			if fabric[xIndex][yIndex] == 0 {
 				fabric[xIndex][yIndex] = 1
-			} else {
+			} else if fabric[xIndex][yIndex] == 1 {
 				fabric[xIndex][yIndex] = 2
 			}
 		}
@@ -70,19 +89,35 @@ func addClaimToFabric(fabric [][]int, claim Claim) [][]int {
 
 func makeEmptyFabric() [][]int {
 	fabric := [][]int{}
-	row := []int{}
 	for i := 0; i < 1000; i++ {
-		row = append(row, 0)
-	}
-	for i := 0; i < 1000; i++ {
-		fabric = append(fabric, row)
+		column := []int{}
+		for j := 0; j < 1000; j++ {
+			column = append(column, 0)
+		}
+		fabric = append(fabric, column)
 	}
 	return fabric
 }
 
 func printFabric(fabric [][]int) {
-	for x := 0; x < len(fabric); x++ {
-		fmt.Printf("Line: %d\n", x)
-		fmt.Println(fabric[x])
+	for y := 0; y < len(fabric[0]); y++ {
+		// fmt.Printf("\nLine: %d", y)
+		fmt.Println()
+		for x := 0; x < len(fabric); x++ {
+			fmt.Printf("%d", fabric[x][y])
+		}
 	}
+}
+
+func countMultipleClaims(fabric [][]int) int {
+	countOfMultipleClaims := 0
+	for y := 0; y < len(fabric[0]); y++ {
+		for x := 0; x < len(fabric); x++ {
+			if fabric[x][y] == 2 {
+				countOfMultipleClaims++
+			}
+		}
+	}
+
+	return countOfMultipleClaims
 }

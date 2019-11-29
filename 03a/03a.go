053e438d8d7fc7d4d1d51f63d3b6bef05a33657e
Heather Forbes <heather_forbes@homedepot.com>
2019-11-29 09:23:51 -0600

    Set up 03a. Set up debugging. 03a makes empty fabric correctly.

--- 03a/03a.go
+++ 03a/03a.go
@@ -18,14 +18,71 @@ func main() {
 
 	reader := bufio.NewReader(file)
 
+	fabric := makeEmptyFabric()
+
+	// fmt.Println("Let's try to print the empty fabric:")
+	// printFabric(fabric)
+
 	for {
-		line, _, err := reader.ReadLine()
+		lineBytes, _, err := reader.ReadLine()
+		line := string(lineBytes)
 
 		if err == io.EOF {
 			break
 		}
 
+		claim := makeClaimFromLine(line)
+
+		fabric = addClaimToFabric(fabric, claim)
+
 		fmt.Printf("%s \n", line)
 	}
 
+	// fmt.Printf("\nFinal fabric:\n")
+	// printFabric(fabric)
+
+}
+
+type Claim struct {
+	xOffset, yOffset, xLength, yLength int
+}
+
+func makeClaimFromLine(line string) Claim {
+	// segments := strings.Split(line, string(' '))
+
+	return Claim{3, 2, 5, 4}
+}
+
+func addClaimToFabric(fabric [][]int, claim Claim) [][]int {
+
+	for xIndex := claim.xOffset; xIndex < claim.xOffset+claim.xLength-1; xIndex++ {
+		for yIndex := claim.yOffset; yIndex < claim.yOffset+claim.yLength-1; yIndex++ {
+			if fabric[xIndex][yIndex] == 0 {
+				fabric[xIndex][yIndex] = 1
+			} else {
+				fabric[xIndex][yIndex] = 2
+			}
+		}
+	}
+
+	return fabric
+}
+
+func makeEmptyFabric() [][]int {
+	fabric := [][]int{}
+	row := []int{}
+	for i := 0; i < 1000; i++ {
+		row = append(row, 0)
+	}
+	for i := 0; i < 1000; i++ {
+		fabric = append(fabric, row)
+	}
+	return fabric
+}
+
+func printFabric(fabric [][]int) {
+	for x := 0; x < len(fabric); x++ {
+		fmt.Printf("Line: %d\n", x)
+		fmt.Println(fabric[x])
+	}
 }
